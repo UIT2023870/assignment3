e2f6a52b9382b534b52492425e6cbb4ca97ea771
UIT2023870 <vedika.waghmare@cumminscollege.in>
2026-04-21 14:13:16 +0530

    Add additional greeting message to Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub + Jenkins! + Maven too");
+        System.out.println("Hello, but this is after poll SCM change");
     }
 }
